e03e2b4d80b968cf80e524f8fa64116291cd90f6
Vidit Parikh <62847556+Vidit10@users.noreply.github.com>
2024-11-03 12:23:32 +0530

    Update RotaryEncoder.cpp

--- RotaryEncoder/RotaryEncoder.cpp
+++ RotaryEncoder/RotaryEncoder.cpp
@@ -4,7 +4,8 @@ RotaryEncoder::RotaryEncoder(int pinA, int pinB, int pinSwitch)
     : _pinA(pinA), _pinB(pinB), _pinSwitch(pinSwitch), _encoderPosCount(0), _bCW(true),
       _pressStartTime(0), _buttonHeld(false) {}
 
-void RotaryEncoder::begin() {
+void RotaryEncoder::begin()
+{
     pinMode(_pinA, INPUT);
     pinMode(_pinB, INPUT);
     pinMode(_pinSwitch, INPUT);
@@ -13,14 +14,19 @@ void RotaryEncoder::begin() {
     Serial.println("Encoder initialized");
 }
 
-void RotaryEncoder::update() {
+void RotaryEncoder::update()
+{
     int aVal = digitalRead(_pinA);
-    if (aVal != _pinALast) { // If the knob is rotating
+    if (aVal != _pinALast)
+    { // If the knob is rotating
         // Determine direction by reading pin B
-        if (digitalRead(_pinB) != aVal) { // Clockwise rotation
+        if (digitalRead(_pinB) != aVal)
+        { // Clockwise rotation
             _encoderPosCount++;
             _bCW = true;
-        } else { // Counterclockwise rotation
+        }
+        else
+        { // Counterclockwise rotation
             _encoderPosCount--;
             _bCW = false;
         }
@@ -36,35 +42,51 @@ void RotaryEncoder::update() {
     checkButtonPressDuration();
 }
 
-int RotaryEncoder::getPosition() const {
+int RotaryEncoder::getPosition() const
+{
     return _encoderPosCount;
 }
 
-bool RotaryEncoder::isClockwise() const {
+bool RotaryEncoder::isClockwise() const
+{
     return _bCW;
 }
 
-bool RotaryEncoder::isSwitchPressed() const {
+bool RotaryEncoder::isSwitchPressed() const
+{
     return !digitalRead(_pinSwitch);
 }
 
 // New function to check button press duration
-void RotaryEncoder::checkButtonPressDuration() {
-    if (isSwitchPressed()) { // Button is pressed
-        if (!_buttonHeld) { // First press detection
+int RotaryEncoder::checkButtonPressDuration()
+{
+    int timeReturn = 0;
+    if (isSwitchPressed())
+    { // Button is pressed
+        if (!_buttonHeld)
+        {                               // First press detection
             _pressStartTime = millis(); // Record the time of press
             _buttonHeld = true;
-        } else if (millis() - _pressStartTime >= _longPressThreshold) { // Long press detection
+        }
+        else if (millis() - _pressStartTime >= _longPressThreshold)
+        { // Long press detection
             Serial.println("Button held for more than 2 seconds");
             // Additional actions for long press
         }
-    } else { // Button is not pressed
-        if (_buttonHeld) { // Previously held
-            if (millis() - _pressStartTime < _longPressThreshold) { // Short press detection
+        timeReturn = millis() - _pressStartTime;
+    }
+    else
+    { // Button is not pressed
+        if (_buttonHeld)
+        { // Previously held
+            if (millis() - _pressStartTime < _longPressThreshold)
+            { // Short press detection
                 Serial.println("Button pressed briefly");
                 // Additional actions for short press
             }
             _buttonHeld = false; // Reset the hold state
         }
+        timeReturn = millis() - _pressStartTime;
     }
+    return timeReturn;
 }
